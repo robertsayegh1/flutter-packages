b67c61d6f65c082fa8db69fa0a32b0222a83ff3d
Abdalaziz <alazeez353@gmail.com>
2025-04-14 13:08:56 +0300

    feat(packages): update to latest version

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayerPlugin.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayerPlugin.java
@@ -38,29 +38,29 @@ public class VideoPlayerPlugin implements FlutterPlugin, AndroidVideoPlayerApi {
   /** Register this with the v2 embedding for the plugin to respond to lifecycle callbacks. */
   public VideoPlayerPlugin() {}
 
-  @SuppressWarnings("deprecation")
-  private VideoPlayerPlugin(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    this.flutterState =
-        new FlutterState(
-            registrar.context(),
-            registrar.messenger(),
-            registrar::lookupKeyForAsset,
-            registrar::lookupKeyForAsset,
-            registrar.textures());
-    flutterState.startListening(this, registrar.messenger());
-  }
+//  @SuppressWarnings("deprecation")
+//  private VideoPlayerPlugin(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
+//    this.flutterState =
+//        new FlutterState(
+//            registrar.context(),
+//            registrar.messenger(),
+//            registrar::lookupKeyForAsset,
+//            registrar::lookupKeyForAsset,
+//            registrar.textures());
+//    flutterState.startListening(this, registrar.messenger());
+//  }
 
   /** Registers this with the stable v1 embedding. Will not respond to lifecycle events. */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(
-      @NonNull io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    final VideoPlayerPlugin plugin = new VideoPlayerPlugin(registrar);
-    registrar.addViewDestroyListener(
-        view -> {
-          plugin.onDestroy();
-          return false; // We are not interested in assuming ownership of the NativeView.
-        });
-  }
+//  @SuppressWarnings("deprecation")
+//  public static void registerWith(
+//      @NonNull io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
+//    final VideoPlayerPlugin plugin = new VideoPlayerPlugin(registrar);
+//    registrar.addViewDestroyListener(
+//        view -> {
+//          plugin.onDestroy();
+//          return false; // We are not interested in assuming ownership of the NativeView.
+//        });
+//  }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
